439f3768c57bc2f1c11382b4e971222f2a8e97d7
Albom <o.v.bogomaz1985@gmail.com>
2019-12-02 11:12:24 +0200

    C/C++ support

--- c/include/albom.h
+++ c/include/albom.h
@@ -2,54 +2,53 @@
 #include <stdio.h>
 #include <time.h>
 
-// forward.h
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern "C"  double spectrum_1(double m1, double ti, double te, double freq);
-extern "C"  int acf_1(double m1, double ti, double te, double acf[], int len);
+double spectrum_1(double m1, double ti, double te, double freq);
+int acf_1(double m1, double ti, double te, double acf[], int len);
 
-extern "C"  double spectrum_2(double m1, double m2, double g1, double ti, double te, double freq);
-extern "C"  int acf_2(double m1, double m2, double g1, double ti, double te, double acf[], int len);
+double spectrum_2(double m1, double m2, double g1, double ti, double te, double freq);
+int acf_2(double m1, double m2, double g1, double ti, double te, double acf[], int len);
 
-extern "C"  double spectrum_3(double m1, double m2, double m3, double g1, double g2, double ti, double te, double freq);
-extern "C"  int acf_3(double m1, double m2, double m3, double g1, double g2, double ti, double te, double acf[], int len);
+double spectrum_3(double m1, double m2, double m3, double g1, double g2, double ti, double te, double freq);
+int acf_3(double m1, double m2, double m3, double g1, double g2, double ti, double te, double acf[], int len);
 
-extern "C"  double spectrum_3_full(double m1, double m2, double m3, double g1, double g2, double ti, double te, double ne, int iskD, double freq);
-extern "C"  int acf_3_full(double m1, double m2, double m3, double g1, double g2, double ti, double te, double ne, int iskD, double acf[], int len, double dt);
+double spectrum_3_full(double m1, double m2, double m3, double g1, double g2, double ti, double te, double ne, int iskD, double freq);
+int acf_3_full(double m1, double m2, double m3, double g1, double g2, double ti, double te, double ne, int iskD, double acf[], int len, double dt);
 
-extern "C"  int acf_library_light_short(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
-extern "C"  int acf_library_light_short_conv(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
-extern "C"  int library_light_list_of_temperatures_get(int *temperatures);
-extern "C"  int library_light_list_get(char* prefix, int he_percent_int, char *list);
-extern "C"  int library_light_list_get_filename(char *filename, char *filelist, int num);
+int acf_library_light_short(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
+int acf_library_light_short_conv(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
+int library_light_list_of_temperatures_get(int *temperatures);
+int library_light_list_get(char* prefix, int he_percent_int, char *list);
+int library_light_list_get_filename(char *filename, char *filelist, int num);
 
-extern "C"  int library_heavy_list_of_temperatures_get(int *temperatures);
-extern "C"  int acf_library_heavy(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
+int library_heavy_list_of_temperatures_get(int *temperatures);
+int acf_library_heavy(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
 
-extern "C"  int library_oxygen_list_of_temperatures_get(int *temperatures);
-extern "C"  int acf_library_oxygen(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
+int library_oxygen_list_of_temperatures_get(int *temperatures);
+int acf_library_oxygen(FILE *f, int *temperatures, int temperatures_len, double ti, double te, double *acf, int len);
 
-// mymath.h
-
-
-extern "C" double stat_mean_d
+double stat_mean_d
         (
         double *in,
         int len
         );
 
-extern "C" double stat_deviation_d
+double stat_deviation_d
         (
         double *in,
         int len
         );
 
-extern "C" double stat_variance_d
+double stat_variance_d
         (
         double *in,
         int len
         );
 
-extern "C" int stat_distribution_d
+int stat_distribution_d
         (
         double *in,
         int in_len,
@@ -59,13 +58,13 @@ extern "C" int stat_distribution_d
         double step
         );
 
-extern "C" double  random_rnd(void);
-extern "C" int     random_randomize(int num);
-extern "C" int     random_rdtsc(void);
+double  random_rnd(void);
+int     random_randomize(int num);
+int     random_rdtsc(void);
 
-extern "C" double random_gauss();
+double random_gauss();
 
-extern "C" int	fourier_DFT_d
+int	fourier_DFT_d
         (
 		double *in,
 		int il,
@@ -76,7 +75,7 @@ extern "C" int	fourier_DFT_d
 		double dt
 		);
 
-extern "C" int	fourier_DCT_d
+int	fourier_DCT_d
         (
 		double *in,
 		int il,
@@ -88,7 +87,7 @@ extern "C" int	fourier_DCT_d
 		);
 
 
-extern "C" int	fourier_FFT_d // прямое быстрое преобразование Фурье
+int	fourier_FFT_d // прямое быстрое преобразование Фурье
         (
         double in1[], // входные вещественные
         double in2[], // входные мнимые
@@ -97,7 +96,7 @@ extern "C" int	fourier_FFT_d // прямое быстрое преобразование Фурье
         int len // размер массивов
         );
 
-extern "C" int	fourier_IFFT_d // обратное быстрое преобразование Фурье
+int	fourier_IFFT_d // обратное быстрое преобразование Фурье
         (
         double in1[], // входные вещественные
         double in2[], // входные мнимые
@@ -106,7 +105,7 @@ extern "C" int	fourier_IFFT_d // обратное быстрое преобразование Фурье
         int len // размер массивов
         );
 
-extern "C" int array_sub_d
+int array_sub_d
         (
         double *in1,
         double *in2,
@@ -114,7 +113,7 @@ extern "C" int array_sub_d
         int len
         );
 
-extern "C" int array_add_d
+int array_add_d
         (
         double *in1,
         double *in2,
@@ -122,19 +121,19 @@ extern "C" int array_add_d
         int len
         );
 
-extern "C" double  array_max_d
+double  array_max_d
         (
         double *in,
         int len
         );
 
-extern "C" double  array_min_d
+double  array_min_d
         (
         double *in,
         int len
         );
 
-extern "C" int  array_mul_d
+int  array_mul_d
         (
         double *in1,
         double *in2,
@@ -142,7 +141,7 @@ extern "C" int  array_mul_d
         int len
         );
 
-extern "C" int  array_div_d
+int  array_div_d
         (
         double *in1,
         double *in2,
@@ -150,7 +149,7 @@ extern "C" int  array_div_d
         int len
         );
 
-extern "C" int array_norm_d
+int array_norm_d
         (
         double *in1,
         double *out,
@@ -158,21 +157,21 @@ extern "C" int array_norm_d
         int len
         );
 
-extern "C" int array_norm0_d
+int array_norm0_d
         (
         double *in,
         double *out,
         int len
         );
 
-extern "C" double array_error_d
+double array_error_d
         (
         double *in1,
         double *in2,
         int len
         );
 
-extern "C" double array_linear_d
+double array_linear_d
         (
         double x,
         double *in_x,
@@ -180,7 +179,7 @@ extern "C" double array_linear_d
         int len
         );
 
-extern "C" int array_trand_d
+int array_trand_d
         (
         double *in1,
         double *in2,
@@ -188,7 +187,7 @@ extern "C" int array_trand_d
         int len
         );
 
-extern "C" int func_conv_d
+int func_conv_d
         (
         const double *in1,
         const double *in2,
@@ -196,7 +195,7 @@ extern "C" int func_conv_d
         int len
         );
 
-extern "C" int spline_bspline3_d
+int spline_bspline3_d
         (
         double *in_x,
         double *in_y,
@@ -206,7 +205,7 @@ extern "C" int spline_bspline3_d
         int out_len
         );
 
-extern "C" int approx_poly_coeff_d
+int approx_poly_coeff_d
         (
          double *in_x,
          double *in_y,
@@ -215,102 +214,103 @@ extern "C" int approx_poly_coeff_d
          int N
          );
 
-extern "C" int filelist_get(char *dir_name, char *filelist);
-extern "C" int filelist_get_filename(char *filelist, char *filename, int num);
+int filelist_get(char *dir_name, char *filelist);
+int filelist_get_filename(char *filelist, char *filename, int num);
 
-extern "C" int file_add_i (
+int file_add_i (
                     char *fname,
                     int *in,
                     int il,
                     char *format
                     );
 
-extern "C" int file_add_d(
+int file_add_d(
         char *fname,
         double *in,
         int il,
         char *format
         );
 
-extern "C" int file_newline (
+int file_newline (
         char *fname
         );
 
-extern "C" int file_creat (
+int file_creat (
         char *fname
         );
 
-extern "C" int file_creat_and_add_s(
+int file_creat_and_add_s(
         char *fname,
         char *in
         );
 
-extern "C" int file_add_s(
+int file_add_s(
         char *fname,
         char *in,
         char *format
         );
 
-extern "C" int file_add_bin_d(
+int file_add_bin_d(
         char *fname,
         double *in,
         int il
         );
 
-extern "C" int file_load_all_d (
+int file_load_all_d (
         char *fname,
         double *buf
         );
 
-extern "C" int file_size (char *fname);
+int file_size (char *fname);
 
-extern "C" int buffer_clear(char *buf);
+int buffer_clear(char *buf);
 
-extern "C" int  buffer_newline (
+int  buffer_newline (
         char *buf
         );
 
-extern "C" int buffer_add_s(
+int buffer_add_s(
         char *buf,
         char *in,
         char *format
         );
 
-extern "C" int file_add_sh(
+int file_add_sh(
         char *fname,
         short *in,
         int il,
         char *format
         );
 
-extern "C" int buffer_add_i(
+int buffer_add_i(
         char *buf,
         int *in,
         int il,
         char *format
         );
 
-extern "C" int buffer_add_d(
+int buffer_add_d(
         char *buf,
         double *in,
         int il,
         char *format
         );
 
+time_t date_2unixtime(int DD, int MM, int YY, int hh, int mm, int ss);
+int unixtime_2date(time_t timestamp, int *DD, int *MM, int *YY, int *hh, int *mm, int *ss);
+double time_2decimal(int hh, int mm, int ss);
+int time_2str(int hh, int mm, int ss, char *timestr);
+int date_2str(int dd, int mm, int yy, char *datestr);
+int time_2sec(int hh, int mm, int ss);
+int time_accum_num(int min);
+int date_year_leap(int year);
+int date_next(int *day, int *month, int *year);
+int date_valid(int day, int month, int year);
+int time_from_str(int *hh, int *mm, int *ss, const char *timestr);
+void tdatetime_2date(double tdatetime, int *DD, int *MM, int *YY, int *hh, int *mm, int *ss, int *ms);
+int time_linear(double *arr, int length);
+int time_normalize(double *arr, int length);
 
-// mytime.h		
-		
-extern "C"  time_t date_2unixtime(int DD, int MM, int YY, int hh, int mm, int ss);
-extern "C"  int unixtime_2date(time_t timestamp, int *DD, int *MM, int *YY, int *hh, int *mm, int *ss);
-extern "C"  double time_2decimal(int hh, int mm, int ss);
-extern "C"  int time_2str(int hh, int mm, int ss, char *timestr);
-extern "C"  int date_2str(int dd, int mm, int yy, char *datestr);
-extern "C"  int time_2sec(int hh, int mm, int ss);
-extern "C"  int time_accum_num(int min);
-extern "C"  int date_year_leap(int year);
-extern "C"  int date_next(int *day, int *month, int *year);
-extern "C"  int date_valid(int day, int month, int year);
-extern "C"  int time_from_str(int *hh, int *mm, int *ss, const char *timestr);
-extern "C"  void tdatetime_2date(double tdatetime, int *DD, int *MM, int *YY, int *hh, int *mm, int *ss, int *ms);
-extern "C"  int time_linear(double *arr, int length);
-extern "C"  int time_normalize(double *arr, int length);
+#ifdef __cplusplus
+}
+#endif
